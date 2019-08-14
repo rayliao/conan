6d9f6ced1aaac7458d66b50077d9788630c3336c
rayliao <littlefish@littlefishdeiMac.local>
2019-08-14 10:01:21 +0800

    chore

--- test.py
+++ test.py
@@ -1,38 +1,17 @@
-class Dict(dict):
-    '''
-    Simple dict but also support access as x.y style.
+import json
 
-    >>> d1 = Dict()
-    >>> d1['x'] = 100
-    >>> d1.x
-    100
-    >>> d1.y = 200
-    >>> d1['y']
-    200
-    >>> d2 = Dict(a=1, b=2, c='3')
-    >>> d2.c
-    '3'
-    >>> d2['empty']
-    Traceback (most recent call last):
-        ...
-    KeyError: 'empty'
-    >>> d2.empty
-    Traceback (most recent call last):
-        ...
-    AttributeError: 'Dict' object has no attribute 'empty'
-    '''
-    def __init__(self, **kw):
-        super(Dict, self).__init__(**kw)
 
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+def file():
+    with open('./result.txt', 'a') as f:
+        f.write('new line')
 
-    def __setattr__(self, key, value):
-        self[key] = value
 
-if __name__=='__main__':
-    import doctest
-    doctest.testmod()
+def demo():
+    with open('./result.txt') as f:
+        for line in f.readlines():
+            l = json.loads(line)
+            print(l['title'])
+
+
+if __name__ == "__main__":
+    demo()

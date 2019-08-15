b2d1a2d7f951b1bc3907a8217bb8eb3d12e9466e
rayliao <littlefish@littlefishdeiMac.local>
2019-08-15 14:52:07 +0800

    feat: 下载豆瓣相册内容 #1

--- login.py
+++ login.py
@@ -11,27 +11,53 @@ class Login(object):
         self.login_url = 'https://accounts.douban.com/j/mobile/login/basic'
         self.logined_url = 'https://www.douban.com/photos/album/1683694782'
         self.session = requests.Session()
+        self.index = 0
 
-    def enter(self, email, pwd):
-        # post_data = {
-        #     'ck': '',
-        #     'name': email,
-        #     'password': pwd,
-        #     'remember': 'false',
-        #     'ticket': ''
-        # }
-        response = self.session.post(
-            'https://www.douban.com', headers=self.headers)
+    def login(self, email, pwd):
+        post_data = {
+            'ck': '',
+            'name': email,
+            'password': pwd,
+            'remember': 'false',
+            'ticket': ''
+        }
+        self.session.post(self.login_url, data=post_data, headers=self.headers)
+
+    def enter(self, url=None):
+        if url is None:
+            url = self.logined_url
+        response = self.session.get(url, headers=self.headers)
         if response.status_code == 200:
             html = etree.HTML(response.text)
-            result = html.xpath('//div[@class="albums"]//li/a/text()')
-            print(result)
-        # self.session.post(self.login_url, data=post_data, headers=self.headers)
+            photolst = html.xpath('//div[@class="photo_wrap"]/a/@href')
+            self.get_item(photolst)
+            nextPage = html.xpath('//link[@rel="next"]/@href')
+            if len(nextPage) > 0:
+                self.enter(nextPage[0])
 
-        # response = self.session.get(self.logined_url, headers=self.headers)
-        # html = etree.HTML(response.text)
+    # 获取单张相片信息
+    def get_item(self, lst):
+        for item in lst:
+            response = self.session.get(item, headers=self.headers)
+            html = etree.HTML(response.text)
+            display = html.xpath(
+                '//div[@class="photo_descri"]//span[@id="display"]/text()')
+            src = html.xpath('//div[@class="image-show"]//img/@src')
+            if len(src) > 0:
+                self.save_img(src[0])
+            if len(display) > 0:
+                with open('./album/title.txt', 'a') as f:
+                    f.write(display[0] + '\n')
+
+    # 保存图片
+    def save_img(self, src):
+        html = requests.get(src)
+        with open('./album/' + str(self.index) + '.jpg', 'wb') as f:
+            f.write(html.content)
+        self.index += 1
 
 
 if __name__ == '__main__':
     login = Login()
-    login.enter(email='574954033@qq.com', pwd='wishes--931121')
+    login.login(email='574954033@qq.com', pwd='wishes--931121')
+    login.enter()

6fd3e011875787a7d38e3ea2d6b13fc0a3e73748
rayliao <littlefish@littlefishdeiMac.local>
2019-08-21 15:33:57 +0800

    feat: 使用selenium登录微博 #2

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import json
+import requests
+from lxml import etree
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def file():
@@ -13,9 +19,43 @@ def demo():
             l = json.loads(line)
             print(l['title'])
 
+
 def browser():
-    webdriver.Chrome()
+    try:
+        b = webdriver.Chrome()
+        url = 'https://weibo.com/'
+        b.get(url)
+        wait = WebDriverWait(b, 10)
+        wait.until(EC.url_to_be(url))
+        name = b.find_element_by_id('loginname')
+        pwd = b.find_element_by_name('password')
+        name.send_keys('574954033@qq.com')
+        pwd.send_keys('931121--wishes')
+        submit = b.find_element_by_xpath('//a[@action-type="btn_submit"]')
+        submit.click()
+        print(submit)
+    finally:
+        pass
 
 
-if __name__ == "__main__":
-    browser()
+if __name__ == '__main__':
+    b = webdriver.Chrome()
+    try:
+        url = 'https://weibo.com/'
+        b.get(url)
+        wait = WebDriverWait(b, 10)
+        wait.until(EC.url_to_be(url))
+        name = b.find_element_by_id('loginname')
+        pwd = b.find_element_by_name('password')
+        name.send_keys('574954033@qq.com')
+        pwd.send_keys('wishes--891002')
+        submit = b.find_element_by_xpath('//a[@action-type="btn_submit"]')
+        submit.click()
+        wait.until(EC.presence_of_element_located((By.ID, 'v6_pl_content_publishertop')))
+        input = b.find_element_by_xpath('//textarea[@node-type="textEl"]')
+        input.send_keys('publish this message by python using selenium with chromedriver, hooray!')
+        btn = b.find_element_by_xpath('//a[@node-type="submit"]')
+        btn.click()
+        pass
+    finally:
+        pass
